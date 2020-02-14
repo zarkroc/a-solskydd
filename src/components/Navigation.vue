e991c59c8cc4f1406967657fd3c020702a8a5906
Tomas Perers <9654120+zarkroc@users.noreply.github.com>
2020-02-14 14:51:38 +0100

    added code to hide navbar (need changes)

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -28,3 +28,41 @@
     </ul>
   </header>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      showNavbar: true,
+      lastScrollPosition: 0,
+      scrollValue: 0
+    };
+  },
+
+  mounted() {
+    this.lastScrollPosition = window.pageYOffset;
+    window.addEventListener("scroll", this.onScroll);
+    const viewportMeta = document.createElement("meta");
+    viewportMeta.name = "viewport";
+    viewportMeta.content = "width=device-width, initial-scale=1";
+    document.head.appendChild(viewportMeta);
+  },
+
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.onScroll);
+  },
+
+  methods: {
+    onScroll() {
+      if (window.pageYOffset < 0) {
+        return;
+      }
+      if (Math.abs(window.pageYOffset - this.lastScrollPosition) < 60) {
+        return;
+      }
+      this.showNavbar = window.pageYOffset < this.lastScrollPosition;
+      this.lastScrollPosition = window.pageYOffset;
+    }
+  }
+};
+</script>
