2e854d5d77e71dc6a34bfc19166902212d7e3aeb
zarkroc <9654120+zarkroc@users.noreply.github.com>
2020-02-15 18:49:49 +0100

    Showing product-meny in sidebar

--- src/components/inomhus/InomhusProducts.vue
+++ src/components/inomhus/InomhusProducts.vue
@@ -1,13 +1,76 @@
 <template>
-  <section class="products">
-    <router-link to="/products/inomhus/persienner">Persienner</router-link>
-    <router-link to="/products/inomhus/plisseer">Plissèer</router-link>
-    <router-link to="/products/inomhus/rullgardiner">Rullgardiner</router-link>
-    <router-link to="/products/inomhus/solfilm">Solfilm</router-link>
-    <router-link to="/products/inomhus/insektsnet">Insektsnät</router-link>
-    <router-link to="/products/inomhus/lamellgardiner"
-      >Lamellgardiner</router-link
-    >
-    <router-link to="/products/inomhus/duetter">Duetter</router-link>
+  <section>
+    <aside class="products">
+      <button v-on:click="clickProduct('Persienner')">Persienner</button>
+      <div v-if="showPersienner">
+        <Persienner @clicked="showComponent" />
+      </div>
+      <button v-on:click="showPlisseer = !showPlisseer">Plissèer</button>
+      <div v-if="showPlisseer">
+        <Plisseer @clicked="showComponent" />
+      </div>
+
+      <button v-on:click="showRullgardin = !showRullgardin">
+        Rullgardiner
+      </button>
+      <div v-if="showRullgardin">
+        <Rullgardin @clicked="showComponent" />
+      </div>
+      <button v-on:click="showSolfilm = !showSolfilm">Solfilm</button>
+      <div v-if="showSolfilm">
+        <Rullgardin @clicked="showComponent" />
+      </div>
+      <button v-on:click="showInsectsNet = !showInsectsNet">Insektsnät</button>
+      <div v-if="showInsectsNet">
+        <InsectsNet @clicked="showComponent" />
+      </div>
+      <button v-on:click="showLamellGardiner = !showLamellGardiner">
+        Lamellgardiner
+      </button>
+      <div v-if="showLamellGardiner">
+        <LamellGardiner @clicked="showComponent" />
+      </div>
+      <button v-on:click="showDuetter = !showDuetter">Duetter</button>
+      <div v-if="showDuetter">
+        <Duetter @clicked="showComponent" />
+      </div>
+      >
+    </aside>
+    <section class="product-container">
+      <div v-if="showProduct">
+        <component v-bind:is="dynamicComponent"></component>
+      </div>
+    </section>
   </section>
 </template>
+
+<script>
+import Persienner from "@/components/inomhus/Persienner";
+
+export default {
+  components: {
+    Persienner
+  },
+  data() {
+    return {
+      showPersienner: false,
+      showProduct: false,
+      showPlisse: false,
+      dynamicComponent: {
+        template: "<p>wheee</p>"
+      }
+    };
+  },
+  methods: {
+    showComponent(comp) {
+      this.showProduct = true;
+      this.dynamicComponent = comp;
+    },
+    clickProduct(productType) {
+      this.dynamicComponent = null;
+      this.showProduct = false;
+      this["show" + productType] = !this["show" + productType];
+    }
+  }
+};
+</script>
--- src/components/inomhus/Persienner.vue
+++ src/components/inomhus/Persienner.vue
@@ -1,44 +1,24 @@
 <template>
-  <section class="products-persienner">
-    <aside>
-      <button v-on:click="resetShow('16mm')">
-        16mm
-      </button>
-      <button v-on:click="resetShow('25mm')">
-        25mm
-      </button>
-      <button v-on:click="resetShow('35mm')">
-        35mm
-      </button>
-      <button v-on:click="resetShow('50mm')">
-        50mm
-      </button>
-      <button v-on:click="resetShow('Wood')">
-        Trä
-      </button>
-      <button v-on:click="resetShow('NoHole')">
-        No Hole
-      </button>
-    </aside>
-    <section class="product-container" v-if="show16mm">
-      <Persienner16mm />
-    </section>
-    <section class="product-container" v-else-if="show25mm">
-      <Persienner25mm />
-    </section>
-    <section class="product-container" v-else-if="show35mm">
-      <Persienner35mm />
-    </section>
-    <section class="product-container" v-else-if="show50mm">
-      <Persienner50mm />
-    </section>
-    <section class="product-container" v-else-if="showWood">
-      <Wood />
-    </section>
-    <section class="product-container" v-else-if="showNoHole">
-      <NoHole />
-    </section>
-  </section>
+  <div>
+    <button v-on:click="resetShow('16mm')">
+      16mm
+    </button>
+    <button v-on:click="resetShow('25mm')">
+      25mm
+    </button>
+    <button v-on:click="resetShow('35mm')">
+      35mm
+    </button>
+    <button v-on:click="resetShow('50mm')">
+      50mm
+    </button>
+    <button v-on:click="resetShow('Wood')">
+      Trä
+    </button>
+    <button v-on:click="resetShow('NoHole')">
+      No Hole
+    </button>
+  </div>
 </template>
 
 <script>
@@ -50,48 +30,20 @@ import Wood from "@/components/inomhus/persienner/Wood";
 import NoHole from "@/components/inomhus/persienner/NoHole";
 
 export default {
-  data() {
-    return {
-      show16mm: false,
-      show25mm: false,
-      show35mm: false,
-      show50mm: false,
-      showWood: false,
-      showNoHole: false
-    };
-  },
-  components: {
-    Persienner16mm,
-    Persienner25mm,
-    Persienner35mm,
-    Persienner50mm,
-    Wood,
-    NoHole
-  },
   methods: {
     resetShow: function(toShow) {
-      if (this["show" + toShow] == true) {
-        this["show" + toShow] = false;
-        return;
-      }
-      this.show16mm = false;
-      this.show25mm = false;
-      this.show35mm = false;
-      this.show50mm = false;
-      this.showWood = false;
-      this.showNoHole = false;
       if (toShow == "16mm") {
-        this.show16mm = true;
+        this.$emit("clicked", Persienner16mm);
       } else if (toShow == "25mm") {
-        this.show25mm = true;
+        this.$emit("clicked", Persienner25mm);
       } else if (toShow == "35mm") {
-        this.show35mm = true;
+        this.$emit("clicked", Persienner35mm);
       } else if (toShow == "50mm") {
-        this.show50mm = true;
+        this.$emit("clicked", Persienner50mm);
       } else if (toShow == "Wood") {
-        this.showWood = true;
+        this.$emit("clicked", Wood);
       } else if (toShow == "NoHole") {
-        this.showNoHole = true;
+        this.$emit("clicked", NoHole);
       }
     }
   }
