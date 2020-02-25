1805c6bf59fcff15e0f65f9c238f687103565ac7
Tomas Perers <9654120+zarkroc@users.noreply.github.com>
2020-02-25 12:12:00 +0100

    Added cypress and configuration files

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    css: {
-        loaderOptions: {
-            sass: {
-                prependData: `@import "@/style/application.scss";`
-            }
-        }
-    }
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `@import "@/style/application.scss";`,
+      },
+    },
+  },
 };
