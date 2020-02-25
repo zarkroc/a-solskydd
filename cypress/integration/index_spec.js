b5c8300dbef8f4bbb17f72b0298eb9b26dae5de0
Tomas Perers <9654120+zarkroc@users.noreply.github.com>
2020-02-25 13:45:38 +0100

    Added some simple cypress test cases

--- cypress/integration/index_spec.js
+++ cypress/integration/index_spec.js
@@ -6,19 +6,31 @@ context('Window', () => {
   });
 
   it('cy.window() - get the global window object', () => {
-    // https://on.cypress.io/window
     cy.window().should('have.property', 'top');
   });
 
   it('cy.document() - get the document object', () => {
-    // https://on.cypress.io/document
     cy.document()
       .should('have.property', 'charset')
       .and('eq', 'UTF-8');
   });
 
   it('cy.title() - get the title', () => {
-    // https://on.cypress.io/title
     cy.title().should('include', 'a-solskydd');
+  });
+
+  it('cy.get(h2) - Get the heading', () => {
+    cy.get('h2').should('contain', 'Om oss');
+    cy.get('h2').should('contain', 'Offer');
+    cy.get('h2').should('contain', 'Utvändiga produkter');
+    cy.get('h2').should('contain', 'Invändiga produkter');
+    cy.get('h2').should('contain', 'Leverantörer');
+    cy.get('h2').should('contain', 'Sociala medier');
+    cy.get('h2').should('contain', 'Kontakta oss');
+  });
+  it('fill form', () => {
+    cy.get('input[type=text').type('Förnamn Efternamn');
+    cy.get('input[type=email').type('fornamn@efternamn.se');
+    cy.get('textarea').type('Det här är ett meddelande');
   });
 });
