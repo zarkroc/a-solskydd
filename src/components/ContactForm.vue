3ec542f5df2b64bc790ad4fdfe887dd967fe966e
Tomas Perers <9654120+zarkroc@users.noreply.github.com>
2020-02-26 13:45:15 +0100

    Validateion of contact form

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -12,17 +12,23 @@
         <ul>
           <li>
             <label for="name">*Namn:</label>
-            <input type="text" name="name" required />
+            <input type="text" name="name" required v-model="message.name" />
             <span>Skriv ditt namn</span>
           </li>
           <li>
             <label for="mail">*E-post:</label>
-            <input type="email" name="mail" required />
+            <input type="email" name="email" required v-model="email.value" />
             <span>Skriv din e-postadress</span>
           </li>
           <li>
             <label for="message">*Meddelande:</label>
-            <textarea name="meddelande" rows="8" cols="80" required></textarea>
+            <textarea
+              name="meddelande"
+              rows="8"
+              cols="80"
+              required
+              v-model="message.msg"
+            ></textarea>
             <span>Skriv ditt meddelande</span>
           </li>
           <li>
@@ -59,9 +65,15 @@
 
 <script>
 export default {
-  name: "Suppliers",
+  name: "ContactForm",
   props: {
     msg: String,
   },
+  data() {
+    return {
+      errors: [],
+    };
+  },
+  methods: {},
 };
 </script>
