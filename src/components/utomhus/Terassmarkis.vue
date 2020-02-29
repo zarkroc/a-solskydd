64b4df79689105ec165f496893c3b7003e09ca5e
zarkroc <9654120+zarkroc@users.noreply.github.com>
2020-02-29 12:37:38 +0100

    removed product

--- src/components/utomhus/Terassmarkis.vue
+++ src/components/utomhus/Terassmarkis.vue
@@ -18,12 +18,6 @@
     >
       Terassmarkis FA62/64
     </button>
-    <button
-      v-on:click="resetShow('MarkisMt64', $event)"
-      :class="{ highlight: selected == 'MarkisMt64' }"
-    >
-      Markis MT64
-    </button>
   </div>
 </template>
 
@@ -31,7 +25,6 @@
 import TerassmarkisFA22 from "@/components/utomhus/markiser/TerassmarkisFA22";
 import TerassmarkisFA40 from "@/components/utomhus/markiser/TerassmarkisFA40";
 import TerassmarkisFA62 from "@/components/utomhus/markiser/TerassmarkisFA62";
-import MarkisMt64 from "@/components/utomhus/markiser/MarkisMt64";
 
 export default {
   data() {
@@ -47,8 +40,6 @@ export default {
         this.$emit("clicked", TerassmarkisFA40);
       } else if (toShow == "TerassmarkisFA62") {
         this.$emit("clicked", TerassmarkisFA62);
-      } else if (toShow == "MarkisMt64") {
-        this.$emit("clicked", MarkisMt64);
       }
       this.selected = toShow;
     },
