807ed1b725b0c11f7b1f22e03f8d5287a17c2bc7
Tomas Perers <9654120+zarkroc@users.noreply.github.com>
2020-03-09 08:33:19 +0100

    Added some gallery things

---
+++ src/components/Gallery.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="about_container">
+    <img :src="require( `../assets/icons/${this.icon}.svg`)"/>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Gallery",
+  computed: {
+    referenceImage() {
+      if (!this.selectedImage) {
+        return;
+      }
+
+      const fileName = this.selectedImage.toLowerCase();
+
+      return require(`../assets/gallery/${fileName}.jpg`);
+    },
+  },
+};
+</script>
