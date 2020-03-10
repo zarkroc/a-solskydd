566827a1801a844a4d44127126d2a8bedc82c4e6
Tomas Perers <9654120+zarkroc@users.noreply.github.com>
2020-03-10 09:29:03 +0100

    Installed gallery plugin and stored images

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,19 +1,35 @@
 <template>
   <section class="gallery">
-    <p>{{ images }}</p>
+    <gallery :images="images" :index="index" @close="index = null"></gallery>
+    <div
+      class="image"
+      v-for="(image, imageIndex) in images"
+      :key="imageIndex"
+      @click="index = imageIndex"
+      :style="{
+        backgroundImage: 'url(' + image + ')',
+        width: '300px',
+        height: '200px',
+      }"
+    ></div>
   </section>
 </template>
 
 <script>
 import axios from "axios";
+import VueGallery from "vue-gallery";
 
 export default {
   name: "Gallery",
   data() {
     return {
-      images: {},
+      images: [],
       errors: [],
+      index: null,
     };
+  },
+  components: {
+    gallery: VueGallery,
   },
   created() {
     var apiHost = "";
@@ -27,6 +43,9 @@ export default {
       .then(response => {
         // JSON responses are automatically parsed.
         this.images = response.data;
+        this.images.forEach(function(image, index, images) {
+          images[index] = `${apiHost}/${image}`;
+        });
       })
       .catch(e => {
         this.errors.push(e.response);
@@ -34,3 +53,13 @@ export default {
   },
 };
 </script>
+<style scoped>
+.image {
+  float: left;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+  border: 1px solid #ebebeb;
+  margin: 5px;
+}
+</style>
